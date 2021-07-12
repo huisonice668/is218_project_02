75deb3182588124b9730e746f33246923bf05f6f
zihui <zb4@gmail.com>
2021-07-12 14:55:04 -0400

    Feature 3: Added requirements and setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version = '0.0.1',
+      description = 'Its a calculator',
+      author = 'ZH',
+      author_email = '',
+      url = '',
+      packages=find_packages(),
+      )
+
+
+
