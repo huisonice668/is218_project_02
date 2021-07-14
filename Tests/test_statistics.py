140dd585bda54417a33c38dbed7f264cbe1ef8ee
zihui <zb4@gmail.com>
2021-07-14 12:45:01 -0400

    Feature 19: Modified unit test for mean function so that it can use randomly generated low, high, count, and seed value to generate the integer list and float list

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -8,21 +8,38 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.statistics = Statistics()
+        rand = GenerateRandom()
 
     def test_instantiate_statisctis(self):
         self.assertIsInstance(self.statistics, Statistics)
 
+    #test mean by generate random low, high, seed value, and number of values
+    # test case 1: with seed, integers list
+    # test case 2: with seed, float list
     def test_mean(self):
         rand = GenerateRandom()
-        randomIntList = rand.with_seed_integers_list(2, 10, 6, 2,)
-        myResult = self.statistics.mean(randomIntList)
-        realResult = statistics.mean(randomIntList)
-        self.assertEqual(myResult, realResult)
+        for i in range(2):
+            rand_low = rand.no_seed_integers(0,10)
+            rand_high = rand.no_seed_integers(11, 20)
+            rand_seed = rand.no_seed_integers(1, 5)
+            rand_count = rand.no_seed_integers(0, 20)
 
+            # test case 1: integer list
+            if i == 0:
+                randomIntList = rand.with_seed_integers_list(rand_low, rand_high, rand_count, rand_seed)
+
+            # test case 2: float list; results of mean will be rounded to 8 digits
+            if i == 1:
+                randomIntList = rand.with_seed_floats_list(rand_low, rand_high, rand_count, rand_seed)
+
+            myResult = round(self.statistics.mean(randomIntList), 8)
+            realResult = round(statistics.mean(randomIntList), 8)
+            self.assertEqual(myResult, realResult)
 
 
     def test_median(self):
         pass
+
     def test_sample_mean(self):
         pass
 
