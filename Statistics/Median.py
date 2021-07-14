1cce461aa4545df655ef0f9a682ae97d760b633e
zihui <zb4@gmail.com>
2021-07-14 13:35:24 -0400

    Feature 20: Modified median function; Added unit test for median function so that it can use randomly generated low, high, count, and seed value to generate the integer list and float list; test for empty ability also added

--- Statistics/Median.py
+++ Statistics/Median.py
@@ -1,15 +1,20 @@
-from Statistics.Mean import Mean
+from Statistics.Mean import mean
 
 
 def median(data):
     index = 0
-    list = []
-    data = data.sort()
+    result = []
+    data.sort()
     counter = len(data)
+
+    # throw exception if list is empty
+    if counter == 0:
+        raise ValueError('Can not have an empty list')
+
     if counter % 2 == 0:
-        index = counter / 2
-        list[0] = data[index - 1]
-        list[1] = data[index]
-        return mean(list)
+        index = counter // 2
+        result.append(data[index - 1])
+        result.append(data[index])
+        return mean(result)
     else:
         return data[counter // 2]
--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,5 +1,6 @@
 from Calculator.Calculator import Calculator
 from Statistics.Mean import mean
+from Statistics.Median import median
 from Statistics import SampleMean
 from Statistics.GenerateRandom import GenerateRandom
 from CsvReader.CsvReader import CsvReader
@@ -13,15 +14,18 @@ class Statistics(Calculator):
         #self.data = GenerateRandom.with_seed_integers_list(0, 10, 6, 2)
         super().__init__()
 
-    def sample_mean(self, sample_size):
-        self.result = mean(self.data, sample_size)
-        return self.result
-
     def mean(self, data):
         self.data = data
         self.result = mean(self.data)
         return self.result
 
-    def median(self):
+    def median(self, data):
+        self.data = data
         self.result = median(self.data)
         return self.result
+
+    def sample_mean(self, sample_size):
+        self.result = mean(self.data, sample_size)
+        return self.result
+
+
