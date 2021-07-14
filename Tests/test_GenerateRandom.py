10611bcf207fd46526eda2a9f98756c008638085
zihui <zb4@gmail.com>
2021-07-14 01:06:59 -0400

    Feature 15: Added random function with_seed_integers that generate a random integer between two values with a seed; also added its unit test function

--- Tests/test_GenerateRandom.py
+++ Tests/test_GenerateRandom.py
@@ -12,10 +12,20 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.generateRandom, GenerateRandom)
 
     def test_no_seed_integers(self):
-        print(self.generateRandom.no_seed_integers(0, 10))
+        #print(self.generateRandom.no_seed_integers(0, 10))
+        pass
 
     def test_no_seed_floats(self):
-        print(self.generateRandom.no_seed_floats(0.0, 10.0))
+        #print(self.generateRandom.no_seed_floats(0.0, 10.0))
+        pass
+
+    def test_with_seed_integers(self):
+        for i in range(5):
+            self.assertEqual(self.generateRandom.with_seed_integers(0, 10, 6), self.generateRandom.with_seed_integers(0, 10, 6))
+            self.assertEqual(self.generateRandom.with_seed_integers(20, 99, 8), self.generateRandom.with_seed_integers(20, 99, 8))
+
+
+
 
 
 if __name__ ==  '__main__':
